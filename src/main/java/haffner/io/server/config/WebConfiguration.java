cbf8d0f0642cc6e92bf9479c6be852c9d200faee
florent.haffner <florent.haffner@protonmail.com>
2020-05-13 13:01:27 +0200

    Update CORS

--- src/main/java/haffner/io/server/config/WebConfiguration.java
+++ src/main/java/haffner/io/server/config/WebConfiguration.java
@@ -13,7 +13,7 @@ public class WebConfiguration implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins(
+                registry.addMapping("/**").allowedOrigins(
                         "http://localhost:8000",
                         "https://beta.haffner.io/"
                 );
